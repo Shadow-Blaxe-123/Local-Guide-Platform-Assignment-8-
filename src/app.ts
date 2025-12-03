eb7fcbb28bcd640903365fbd2f0b537ecbcc035b
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-12-04 00:23:22 +0600

    boiler code

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import type { Application, Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
+import router from "./app/routes";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  })
+);
+
+//parser
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "LG Platform API running ...",
+    enivronment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " seconds",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
