37b28010ef881369b69a60c36d8f5a1e684b86cb
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-12-04 00:24:23 +0600

    boiler code

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import ApiError from "../errors/ApiError";
+import status from "http-status";
+import type { NextFunction, Request, Response } from "express";
+import { jwtHelper } from "../helper/jwtHelper";
+import config from "../../config";
+import type { IJWTPayload } from "../interfaces";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.cookies.accessToken;
+      if (!token) {
+        throw new ApiError(status.UNAUTHORIZED, "You are not logged in!");
+      }
+      const verifiedUser = jwtHelper.verifyToken(
+        token,
+        config.jwt.access_token_secret
+      );
+      req.user = verifiedUser as IJWTPayload;
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(status.UNAUTHORIZED, "You are not authorized!");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
